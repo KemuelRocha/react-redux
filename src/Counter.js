f986345a37ebba866b1ed3193fa4b8a1452a7468
KemuelSR <kemuel@weef.com.br>
2024-01-10 19:58:10 -0300

    feat(redux): treinando implementacoes com redux

---
+++ src/Counter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+
+class Counter extends React.Component{
+  
+  increment = () =>{
+    //TODO: Chamar função para incrementar no próprio redux
+  }
+
+  alterarTitulo = () =>{
+    //TODO: Chamar função para alterar o título no redux
+  }
+
+    render(){
+      return (
+        <>
+          <button onClick={this.alterarTitulo}>Alterar título!</button>
+          <button onClick={this.increment}>Incrementa</button>
+          <h2>{this.props.count}</h2>
+          <h3>{this.props.title}</h3>
+        </>
+      )
+    }
+}
+
+const mapStateToProps = state => ({count:state.count, title:state.title});
+
+export default connect(mapStateToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Counter from './Counter';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const initialStates = {
+  count: 50,
+  title: 'Título inicial',
+}
+
+function reducer(state = initialStates, action) {
+  return state;
+}
+
+const store = createStore(reducer);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
